cd223674982c58585b62105ca90c3d256a719793
Shawn Lewis <shlewis@gmail.com>
2011-05-02 13:29:05 -0700

    Listen for ajaxComplete instead of wrapping jQuery.ajax

--- logged-ajax.js
+++ logged-ajax.js
@@ -1,40 +1,52 @@
-// Wrapper around jQuery.ajax that logs ajax requests/responses in the generating page.
-// options can be passed debug_exempt in addition to the standard jQuery options. If 
-// debug_exempt is truthy this function just delegates to jQuery.ajax immediately.
-var ajax = function(options) {
+// This module writes the most recent ajax request/response in your page.
+// Requires jQuery.
+// Expects three page elements with ids debug_ajax_url, debug_ajax_data
+// and debug_ajax_result.
+// Ex:
+//   <input type="text" id="debug_ajax_url" size=100 /><br>
+//   <textarea id="debug_ajax_data" cols=100 rows=20></textarea><br>
+//   <iframe id="debug_ajax_result" width="100%"></iframe>
+
+(function() {
+
+var writeToIframe = function(iframe, text) {
+    // From http://stackoverflow.com/questions/997986/write-elements-into-a-child-iframe-using-javascript-or-jquery
+    iframeWindow = (iframe.contentWindow) ?
+        iframe.contentWindow :
+        (iframe.contentDocument.document) ?
+            iframe.contentDocument.document :
+            iframe.contentDocument;
+    iframeWindow.document.open();
+    iframeWindow.document.write(text);
+    iframeWindow.document.close();
+
+    // resize iframe to fit contents
+    iframe.style.height = '10px';
+    iframe.style.height =
+        iframe.contentWindow.document.body.scrollHeight + 20 + 'px';
+}
+
+// Listens for ajax completion and logs the response in the page.
+// Looks for an extra option to jQuery ajax functions: debug_exempt. If that
+// option is truty ajax_complete is a noop. Useful for excluding ajax
+// requests that fire periodically for example.
+var ajax_complete = function(ev, xhr, options) {
     if (!options.debug_exempt) {
         $('#debug_ajax_url').val(options.url);
         $('#debug_ajax_data').val(options.data);
-        old_complete = options.complete;
-        options.complete = function(jqXHR) {
-            var result = jqXHR.responseText;
-            if (jqXHR.getResponseHeader('content-type') == 'application/json') {
-                result =
-                    '<pre style="word-wrap: break-word; white-space: pre-wrap;">'
-                    + result
-                    + '</pre>';
-            }
-            // From http://stackoverflow.com/questions/997986/write-elements-into-a-child-iframe-using-javascript-or-jquery
-            var ifrm = document.getElementById('debug_ajax_result');
-            ifrm = (ifrm.contentWindow) ?
-                        ifrm.contentWindow :
-                        (ifrm.contentDocument.document) ?
-                            ifrm.contentDocument.document :
-                            ifrm.contentDocument;
-            ifrm.document.open();
-            ifrm.document.write(result);
-            ifrm.document.close();
-
-            // resize iframe to fit contents
-            var ifrm = document.getElementById('debug_ajax_result');
-            ifrm.style.height = '10px';
-            ifrm.style.height =
-                ifrm.contentWindow.document.body.scrollHeight + 20 + 'px';
-
-            if (old_complete) {
-                old_complete(arguments);
-            }
-        };
+        var result = xhr.responseText;
+        if (xhr.getResponseHeader('content-type') == 'application/json') {
+            result =
+                '<pre style="word-wrap: break-word; white-space: pre-wrap;">'
+                + result
+                + '</pre>';
+        }
+        var iframe = $('#debug_ajax_result').get(0);
+        if (iframe) {
+            writeToIframe(iframe, result);
+        }
     }
-    $.ajax(options);
 }
+$('body').ajaxComplete(ajax_complete);
+
+}());
